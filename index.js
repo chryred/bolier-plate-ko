03fec755a4bb86001fff1fa35c1c136e1cd12952
jeongwon <chryred@naver.com>
2022-03-17 00:02:47 +0900

    fifth

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 const { User } = require("./models/User");
+const { url } = require("./mongodb.js");
 
+
+console.log(url);
 // application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}));
 // application/json
 app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://xxxx", {
+mongoose.connect(url, {
     useNewUrlParser: true, useUnifiedTopology: true
 }).then(() => console.log("MongoDB Connected..."))
   .catch(err => console.log(err));
